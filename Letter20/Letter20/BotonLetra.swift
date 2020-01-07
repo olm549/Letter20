39f61bfdfbc13715e112ab7b96217d835081faa2
Aula11 <aula11@iMac-10.local>
2020-01-07 11:38:51 +0100

    fix addAlumno

---
+++ Letter20/Letter20/BotonLetra.swift
@@ -0,0 +1,21 @@
+//
+//  BotonLetra.swift
+//  Letter20
+//
+//  Created by Aula11 on 7/1/20.
+//  Copyright © 2020 ual. All rights reserved.
+//
+
+import UIKit
+
+class BotonLetra: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
